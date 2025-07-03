ee01d390ef0f54a5476ff9def00aa7c07a32dc9a
Super bodik <91775868+superbodik@users.noreply.github.com>
2025-07-04 01:24:38 +0300

    3

--- install-apps.ps1
+++ install-apps.ps1
@@ -33,8 +33,29 @@
         Pause
     }
 }
+function Download-And-Install {
+    param($url, $installerPath)
 
-# Пример функции установки FireFox
+    Write-Host "Скачиваем: $url"
+    # Можно добавить проверку, если нужен прогресс, или заменить Start-BitsTransfer на Invoke-WebRequest
+    Start-BitsTransfer -Source $url -Destination $installerPath
+
+    Write-Host "Устанавливаем: $installerPath"
+    Start-Process -FilePath $installerPath -ArgumentList "/S" -Wait
+
+    Remove-Item $installerPath -Force
+}
+
+function Install-7Zip {
+    if (Get-Command "7z" -ErrorAction SilentlyContinue) {
+        Write-Host "7-Zip уже установлен."
+        return
+    }
+    $url = "https://www.7-zip.org/a/7z2301-x64.exe"
+    $path = "$env:TEMP\7z_setup.exe"
+    Download-And-Install $url $path
+}
+
 function Install-Firefox {
     $url = "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=ru"
     $path = "$env:TEMP\firefox_setup.exe"
@@ -53,6 +74,18 @@ function Install-Steam {
     Download-And-Install $url $path
 }
 
+function Install-Spotify {
+    $url = "https://download.scdn.co/SpotifySetup.exe"
+    $path = "$env:TEMP\spotify_setup.exe"
+    Download-And-Install $url $path
+}
+
+function Install-VSCode {
+    $url = "https://update.code.visualstudio.com/latest/win32-x64-user/stable"
+    $path = "$env:TEMP\vscode_setup.exe"
+    Download-And-Install $url $path
+}
+
 function Install-UnityHub {
     $url = "https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.exe"
     $path = "$env:TEMP\unityhub_setup.exe"
@@ -64,28 +97,74 @@ function Install-Git {
     $path = "$env:TEMP\git_setup.exe"
     Download-And-Install $url $path
 }
+
+function Install-Spicetify {
+    if (-not (Get-Command "spicetify" -ErrorAction SilentlyContinue)) {
+        Invoke-WebRequest -Uri "https://github.com/spicetify/spicetify-cli/releases/latest/download/spicetify-cli-windows-x64.zip" -OutFile "$env:TEMP\spicetify.zip"
+        Expand-Archive "$env:TEMP\spicetify.zip" -DestinationPath "$env:ProgramFiles\spicetify" -Force
+        $spicePath = "$env:ProgramFiles\spicetify"
+        $envPath = [System.Environment]::GetEnvironmentVariable("PATH", "User")
+        if ($envPath -notlike "*$spicePath*") {
+            [System.Environment]::SetEnvironmentVariable("PATH", "$envPath;$spicePath", "User")
+        }
+        Write-Host "Spicetify установлен."
+    } else {
+        Write-Host "Spicetify уже установлен."
+    }
+}
+
+function Run-MAS {
+    Write-Host "Запуск MAS-активации Windows..."
+    irm https://get.activated.win | iex
+}
+
 function Install-All {
-    Install-Git
-    Install-UnityHub
-    Install-Steam
-    Install-Discord
+    Install-7Zip
     Install-Firefox
+    Install-Discord
+    Install-Steam
+    Install-Spotify
+    Install-VSCode
+    Install-UnityHub
+    Install-Git
+    Install-Spicetify
     Run-MAS
 }
 
-do {
-    Show-AppsMenu
-    $choice = Read-Host "Выберите пункт меню (0-7)"
-    switch ($choice) {
-        '1' { Install-7Zip }
-        '2' { Install-NotepadPP }
-        '3' { Install-VSCode }
-        '4' { Install-Spotify }
-        '5' { Install-Spicetify }
-        '6' { Run-MAS }
-        '7' { Install-All }
-        '0' { Show-MainMenu }
-        default { Write-Host "Неверный выбор. Попробуйте ещё раз." -ForegroundColor Red }
+function Show-AppsMenu {
+    while ($true) {
+        Clear-Host
+        Write-Host "====== УСТАНОВКА ПОЛЕЗНЫХ ПРОГРАММ ======" -ForegroundColor Cyan
+        Write-Host "1. Установить 7-Zip"
+        Write-Host "2. Установить FireFox"
+        Write-Host "3. Установить Discord"
+        Write-Host "4. Установить Steam"
+        Write-Host "5. Установить Spotify"
+        Write-Host "===== Программирование ====="
+        Write-Host "6. Установить Visual Studio Code"
+        Write-Host "7. Установить Unity Hub"
+        Write-Host "8. Установить Git"
+        Write-Host "9. Установить Spicetify (если Spotify уже установлен)"
+        Write-Host "10. Активация Windows (MAS)"
+        Write-Host "0. Назад"
+
+        $choice = Read-Host "Выберите пункт меню (0-10)"
+        switch ($choice) {
+            '1' { Install-7Zip }
+            '2' { Install-Firefox }
+            '3' { Install-Discord }
+            '4' { Install-Steam }
+            '5' { Install-Spotify }
+            '6' { Install-VSCode }
+            '7' { Install-UnityHub }
+            '8' { Install-Git }
+            '9' { Install-Spicetify }
+            '10' { Run-MAS }
+            '0' { return }  # Выход из меню назад
+            default { Write-Host "Неверный выбор. Попробуйте ещё раз." -ForegroundColor Red }
+        }
+        Pause
     }
-    Pause
-} while ($true)
+}
+
+Show-AppsMenu
